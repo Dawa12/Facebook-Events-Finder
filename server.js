43b5f32a466bbc673c75f9699d6ba1d6508c0ae2
Dawa Sherpa <dasherpa7@gmail.com>
2016-12-21 12:44:10 -0500

    only will require dotenv if node_env is development

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV == 'development') require('dotenv').config({ silent: true });
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
