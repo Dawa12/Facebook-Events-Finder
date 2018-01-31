e2a5891397eb0453fbc300b2e3f1a3b0b11a868a
Dawa Sherpa <dasherpa7@gmail.com>
2018-01-31 17:05:49 -0500

    remove conditional to always require dotenv file

--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -1,10 +1,16 @@
-if (process.env.NODE_ENV == 'development') require('dotenv').config({ silent: true });
+// if (process.env.NODE_ENV == 'development')
+// require('dotenv').config({ silent: true });
+
+require('dotenv').config({ silent: true });
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.render('index', {
-    appId: process.env.CLIENT_ID,
+    appId: process.env.CLIENT_ID
   });
+
+  console.log('process.env', process.env);
 });
 
 module.exports = router;
