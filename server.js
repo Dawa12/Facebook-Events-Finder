4653fecfbdf05e1f05e99cb708a0ed2c4f582ddb
Dawa Sherpa <dasherpa7@gmail.com>
2018-01-31 17:04:16 -0500

    prettier code align

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-if (process.env.NODE_ENV == 'development') require('dotenv').config({ silent: true });
+if (process.env.NODE_ENV == 'development')
+  require('dotenv').config({ silent: true });
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const eventsRoute = require ('./routes/eventsRoute')
-const postsRoute = require ('./routes/postsRoute')
+const eventsRoute = require('./routes/eventsRoute');
+const postsRoute = require('./routes/postsRoute');
 
 const app = express();
 const port = process.argv[2] || process.env.PORT || 3000;
@@ -12,10 +13,10 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', homeRoute);
-app.use('/', eventsRoute)
-app.use('/posts', postsRoute)
+app.use('/', eventsRoute);
+app.use('/posts', postsRoute);
 
 app.listen(port, () => console.log('Server is running on ', port));
